e6f16e970267aa867732237830947b1376968ae8
lihvarcekmisa <lihvarcekmisa@yahoo.com>
2022-03-16 16:40:11 +0100

    26th commit - Custom Login With Bootstrap

--- spring_boot_video_service_new/src/main/java/com/springboot/videoservicenew/app/config/DemoSecurityConfig.java
+++ spring_boot_video_service_new/src/main/java/com/springboot/videoservicenew/app/config/DemoSecurityConfig.java
@@ -2,16 +2,19 @@ package com.springboot.videoservicenew.app.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter{
 
-
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 
         //add our users for in memory authentication
@@ -23,5 +26,12 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(users.username("susan").password("test123").roles("ADMIN"));
     }
 
-
+    //modifying Spring Security Configuration
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated()
+                .and().formLogin().loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll();
+    }
 }
--- spring_boot_video_service_new/src/main/java/com/springboot/videoservicenew/app/controller/VideoServiceAppController.java
+++ spring_boot_video_service_new/src/main/java/com/springboot/videoservicenew/app/controller/VideoServiceAppController.java
@@ -10,6 +10,7 @@ public class VideoServiceAppController {
 
 	@GetMapping("/")
 	//@GetMapping
+
 	public String showHome(){
 		
 		return "home";
